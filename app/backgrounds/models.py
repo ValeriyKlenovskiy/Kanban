2e6c1ae688b7be996bb2fc0644f90db7a96d3e7e
Valeriy <Vklenovskii@gmail.com>
2024-08-08 00:13:32 +0300

    started backgrounds

---
+++ app/backgrounds/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import ForeignKey, JSON
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Backgrounds(Base):
+    __tablename__ = "backgrounds"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(nullable=False)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    # image: Mapped[] =
